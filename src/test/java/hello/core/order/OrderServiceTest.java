a18a11046e90c3454ad0c553f88f6accf2b40440
P069513 <j.lee@sk.com>
2025-06-16 15:39:34 +0900

    Spring

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -19,7 +19,7 @@ public class OrderServiceTest {
     public void beforeEach(){
         AppConfig appConfig = new AppConfig();
         memberService = appConfig.memberService();
-        orderService =; appConfig.orderService();
+        orderService = appConfig.orderService();
     }
 
     @Test
